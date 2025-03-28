62037756b6f4e08b24ccd6559b7efdfcad8f7294
Victor Caballero <victor.caballero@bi-sim.com.mx>
2025-03-28 01:05:40 -0600

    Postgrest y Spring boot dock 2.5.7

---
+++ src/main/java/support/Tecnologia/config/WebConfig.java
@@ -0,0 +1,17 @@
+package support.Tecnologia.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080", "https://mi-frontend-en-render.onrender.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
