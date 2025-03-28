fe6758ad8a0b181464116612e768162ad3234897
Victor Caballero <victor.caballero@bi-sim.com.mx>
2025-03-28 00:44:34 -0600

    Postgrest y Spring boot dock 2.5.4

---
+++ src/main/java/support/Tecnologia/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package support.Tecnologia.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<String> handleNullPointerException(NullPointerException ex) {
+        return new ResponseEntity<>("Error: Se ha producido un NullPointerException.", HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return new ResponseEntity<>("Error: Argumento inválido.", HttpStatus.BAD_REQUEST);
+    }
+
+
+
+}
