ac9522bd72d555f7db0e02c60c8d5864b4deeb16
Victor Caballero <victor.caballero@bi-sim.com.mx>
2025-03-27 22:32:36 -0600

    mysql en docker Postgrest  dock 2.4.4

--- src/main/java/support/Tecnologia/TecnologiaApplication.java
+++ src/main/java/support/Tecnologia/TecnologiaApplication.java
@@ -2,9 +2,10 @@ package support.Tecnologia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TecnologiaApplication {
+public class TecnologiaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TecnologiaApplication.class, args);
