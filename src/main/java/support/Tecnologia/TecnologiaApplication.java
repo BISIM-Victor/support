0ee249555a74bf92dec1a9b855e5f10394c8d480
Victor Caballero <victor.caballero@bi-sim.com.mx>
2025-03-31 13:58:20 -0600

    Postgrest conecion con JpaConfig  dock 2.7.8

--- src/main/java/support/Tecnologia/TecnologiaApplication.java
+++ src/main/java/support/Tecnologia/TecnologiaApplication.java
@@ -1,31 +1,15 @@
 package support.Tecnologia;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import support.Tecnologia.config.DatasourceConfig;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class TecnologiaApplication extends SpringBootServletInitializer {
-
-	private final DatasourceConfig datasourceConfig;
-
-	@Autowired
-	public TecnologiaApplication(DatasourceConfig datasourceConfig) {
-		this.datasourceConfig = datasourceConfig;
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TecnologiaApplication.class, args);
-		System.out.println("Aplicación Iniciada...");
+		System.out.println("Aplicacion Iniciada...");
 	}
 
-	@Bean
-	public CommandLineRunner printDatasourceUrl() {
-		return args -> System.out.println("Base de datos configurada: " + datasourceConfig.getDatasourceUrl());
-	}
 }
