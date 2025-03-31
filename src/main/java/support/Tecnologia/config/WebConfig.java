4274a148fdd3c9466faf537f6b657c1e7d5f14b6
Victor Caballero <victor.caballero@bi-sim.com.mx>
2025-03-31 14:09:28 -0600

    Postgrest conecion con JpaConfig  dock 2.7.9

--- src/main/java/support/Tecnologia/config/WebConfig.java
+++ src/main/java/support/Tecnologia/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "https://support-springapp.onrender.com/**")
+                .allowedOrigins("http://localhost:8080", "https://support-spring-app.onrender.com/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
