f640d1b78073113aa1aa65ba0f086b28780ed2f6
Victor Caballero <victor.caballero@bi-sim.com.mx>
2025-03-31 12:29:50 -0600

    Postgrest conecion con JpaConfig  dock 2.7.2

---
+++ src/main/java/support/Tecnologia/config/JpaConfig.java
@@ -0,0 +1,24 @@
+package support.Tecnologia.config;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.activation.DataSource;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "com.example.repository")
+public class JpaConfig {
+    @Bean
+    public DataSource dataSource() {
+        return (DataSource) DataSourceBuilder.create()
+                .url("jdbc:postgresql://dpg-cvgr59qj1k6c738p6lt0-a.oregon-postgres.render.com:5432/support_ge19?sslmode=require")
+                .username("user")
+                .password("G2LYxb1g5HZtvrWG1ZoMAUR1GbQpWjsA")
+                .driverClassName("org.postgresql.Driver")
+                .build();
+    }
+}
