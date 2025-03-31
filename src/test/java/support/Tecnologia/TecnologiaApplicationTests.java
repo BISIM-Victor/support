ebb4894cff36144c883349d72e19cb9cecb1445b
Victor Caballero <victor.caballero@bi-sim.com.mx>
2025-03-31 13:04:00 -0600

    Postgrest conecion con JpaConfig  dock 2.7.6

--- src/test/java/support/Tecnologia/TecnologiaApplicationTests.java
+++ src/test/java/support/Tecnologia/TecnologiaApplicationTests.java
@@ -1,6 +1,5 @@
 package support.Tecnologia;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,7 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 class TecnologiaApplicationTests {
 
 	@Test
-	@Disabled
 	void contextLoads() {
 	}
 
