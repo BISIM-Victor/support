0cb183a44d943b92a9ba8de76071abbfcbbb3035
Victor Caballero <victor.caballero@bi-sim.com.mx>
2025-04-01 01:25:16 -0600

    Postgrest conecion con JpaConfig  dock 2.9.4

--- src/main/java/support/Tecnologia/TecnologiaApplication.java
+++ src/main/java/support/Tecnologia/TecnologiaApplication.java
@@ -2,6 +2,7 @@ package support.Tecnologia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
@@ -9,7 +10,12 @@ public class TecnologiaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TecnologiaApplication.class, args);
-		System.out.println("Aplicacion Iniciada...");
+		System.out.println("Aplicación Iniciada...");
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TecnologiaApplication.class);
+	}
 }
+
